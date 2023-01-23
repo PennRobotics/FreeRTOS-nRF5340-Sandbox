9f6765447c0f74138c799ed82f54c9553978e536
Brian Wright <4408242+PennRobotics@users.noreply.github.com>
2023-01-23 15:22:35 +0100

    Create main.c with placeholders for FreeRTOS LED tasks

---
+++ main.c
@@ -0,0 +1,61 @@
+#include "FreeRTOS.h"
+
+void pvtLED1Setup();  /* 4 ms period */
+void pvtLED2Setup();  /* 100 ms period */
+void pvtLED3Setup();  /* 250 ms period */
+void pvtLED4Setup();  /* 512 ms period */
+
+
+int main(int argc, char **argv)
+{
+  /* TODO: generic board setup */
+  NULL;
+
+  /* TODO: set up GPIO for LEDs */
+  pvtLED1Setup();
+  pvtLED2Setup();
+  pvtLED3Setup();
+  pvtLED4Setup();
+
+  /* TODO: required FreeRTOS setup */
+  NULL;
+
+  /* TODO: create tasks for 4 LEDs */
+  NULL;
+
+  /* TODO: start scheduler */
+  NULL;
+
+  /* TODO: delete task for LED1 after 2 seconds */
+  NULL;
+
+  /* TODO: proper endless loop */
+  NULL;
+
+  for(;;){}  /* TODO: check best practice for unexpected exit from loop */
+  return -1;
+}
+
+
+void pvtLED1Setup()
+{
+  /* TODO */
+}
+
+
+void pvtLED2Setup()
+{
+  /* TODO */
+}
+
+
+void pvtLED3Setup()
+{
+  /* TODO */
+}
+
+
+void pvtLED4Setup()
+{
+  /* TODO */
+}
